f9e0839827d711b8190458a18968f0a5d5a8162a
Joilnen <joilnen@gmail.com>
2021-02-15 14:48:50 -0300

    .

--- src/EngineManager.cpp
+++ src/EngineManager.cpp
@@ -3,6 +3,7 @@
 #include <SDL2/SDL_syswm.h>
 #include <OgreOverlayManager.h>
 #include <OgreOverlaySystem.h>
+#include <OgreRenderTargetListener.h>
 #include "EngineManager.h"
 #include <iostream>
 #include <memory>
@@ -19,50 +20,72 @@ EngineManager::~EngineManager() {
 
 }
 
-bool EngineManager::keyPressed(const OgreBites::KeyboardEvent& evt) {
-
-    if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
-        getRoot()->queueEndRendering();
-    return true;
+void EngineManager::preViewportUpdate (const Ogre::RenderTargetViewportEvent &evt) { 
+    if(!evt.source->getOverlaysEnabled()) return;
+    ImGuiOverlay::NewFrame();
+    ImGui::ShowDemoWindow();
+    Ogre::LogManager::getSingleton().logMessage("PINTEI");
 }
 
-void EngineManager::setup(void) {
+#ifndef OGRE_BUILD_COMPONENT_RTSHADERSYSTEM
+void EngineManager::locateResources() {
+    OgreBites::ApplicationContext::locateResources();
+    // we have to manually specify the shaders
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+        "../resources", "FileSystem", Ogre::ResourceGroupManager::INTERNAL_RESOURCE_GROUP_NAME);
+}
+#endif
+
+void EngineManager::setup() {
 
     OgreBites::ApplicationContext::setup();
+
+    // get a pointer to the already created root
     auto root = getRoot();
     auto scnMgr = root->createSceneManager();
+
+    addInputListener(this);
+    // register our scene with the RTSS
     auto shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
     shadergen->addSceneManager(scnMgr);
-    addInputListener(this);
 
-    auto imguiOverlay = OGRE_NEW Ogre::ImGuiOverlay();
+    // ImGui::CreateContext();
+    auto imguiOverlay = new Ogre::ImGuiOverlay;
     imguiOverlay->setZOrder(300);
-    imguiOverlay->show();
-    OverlayManager::getSingleton().addOverlay(imguiOverlay); 
+	imguiOverlay->show();
+    Ogre::OverlayManager::getSingleton().addOverlay(imguiOverlay);
+
+    auto light = scnMgr->createLight("MainLight");
+    auto lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    lightNode->setPosition(0, 10, 15);
+    lightNode->attachObject(light);
+
+    auto cam = scnMgr->createCamera("myCam");
+    cam->setNearClipDistance(5); // specific to this sample
+    cam->setAutoAspectRatio(true);
+    getRenderWindow()->addViewport(cam);
+
+    mImguiListener.reset(new OgreBites::ImGuiInputListener);
+    mListenerChain = OgreBites::InputListenerChain({mImguiListener.get()});
+
+    auto camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    camNode->setPosition(0, 0, 15);
+    camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+    camNode->attachObject(cam);
+
+    auto ent = scnMgr->createEntity("Sinbad.mesh");
+    auto node = scnMgr->getRootSceneNode()->createChildSceneNode();
+    node->attachObject(ent);
 }
 
-void EngineManager::_init(const std::string &pcfg, const std::string &ocfg) {
+bool EngineManager::keyPressed(const OgreBites::KeyboardEvent& evt) {
 
-}
+    if (evt.keysym.sym == OgreBites::SDLK_ESCAPE) {
+        getRoot()->queueEndRendering();
+        return true;
+    }
 
-void EngineManager::init(const std::string &pcfg, const std::string &ocfg) { 
-
-}
-
-void EngineManager::setResources() {
-
-}
-
-void EngineManager::_createParticle() {
-
-}
-
-void EngineManager::_addLights() {
-
-}
-
-void EngineManager::_initSceneManager() {
-
+    return false;
 }
 
 
--- src/EngineManager.h
+++ src/EngineManager.h
@@ -3,23 +3,27 @@
 
 #include <OGRE/Ogre.h>
 #include <Bites/OgreApplicationContext.h>
+#include <Overlay/OgreOverlaySystem.h>
 #include <OgreRTShaderSystem.h>
 #include "RgeGui.h"
 
-class EngineManager : public OgreBites::ApplicationContext, public OgreBites::InputListener {
+class EngineManager : public OgreBites::ApplicationContext, public Ogre::RenderTargetListener, public OgreBites::InputListener {
 
-    private:
-        void _init(const std::string &pcfg, const std::string &ocfg);
-        void _createParticle();
-        void _addLights();
-        void _initSceneManager();
-        bool keyPressed(const OgreBites::KeyboardEvent& evt);
-        void setup(void);
+    std::unique_ptr<OgreBites::ImGuiInputListener> mImguiListener;
+	OgreBites::InputListenerChain mListenerChain;
+
     public:
         EngineManager();
         virtual ~EngineManager();
-        void init(const std::string &pcfg, const std::string &ocfg);
-        void setResources();
+
+        void preViewportUpdate (const Ogre::RenderTargetViewportEvent &evt);
+
+#ifndef OGRE_BUILD_COMPONENT_RTSHADERSYSTEM
+        void locateResources() {
+#endif
+        void setup();
+        bool keyPressed(const OgreBites::KeyboardEvent& evt);
+
 };
 
 #endif
